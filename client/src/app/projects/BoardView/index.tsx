df141ba79d5f76da84822b0e95708792422952be
Matrim Pathak <matrimpathak1999@gmail.com>
2024-09-17 00:33:15 +0530

    redirect images to s3

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -170,7 +170,7 @@ const Task = ({ task }: TaskProps) => {
     >
       {task.attachments && task.attachments.length > 0 && (
         <Image
-          src={`/${task.attachments[0].fileURL}`}
+          src={`https://flowboard-s3-images.s3.ap-south-1.amazonaws.com/${task.attachments[0].fileURL}`}
           alt={task.attachments[0].filename}
           width={400}
           height={200}
@@ -218,7 +218,7 @@ const Task = ({ task }: TaskProps) => {
             {task.assignee && (
               <Image
                 key={task.assignee.userId}
-                src={`/${task.assignee.profilePictureUrl!}`}
+                src={`https://flowboard-s3-images.s3.ap-south-1.amazonaws.com/${task.assignee.profilePictureUrl!}`}
                 alt={task.assignee.username}
                 width={30}
                 height={30}
@@ -228,7 +228,7 @@ const Task = ({ task }: TaskProps) => {
             {task.author && (
               <Image
                 key={task.author.userId}
-                src={`/${task.author.profilePictureUrl!}`}
+                src={`https://flowboard-s3-images.s3.ap-south-1.amazonaws.com/${task.author.profilePictureUrl!}`}
                 alt={task.author.username}
                 width={30}
                 height={30}
