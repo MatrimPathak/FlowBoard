14000da9e9bd3067dd512c638feb35a6c15917db
Matrim Pathak <matrimpathak1999@gmail.com>
2024-09-17 14:30:15 +0530

    created user routes post trigger

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -46,7 +46,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mt-5">
       <Authenticator formFields={formFields}>
-        {({ user }: any) =>
+        {({ user }: { user: CognitoUser | null }) =>
           user ? (
             <div className="">{children}</div>
           ) : (
--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -13,3 +13,27 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
 		});
 	}
 };
+
+export const postUSer = async (req: Request, res: Response) => {
+	try {
+		const {
+			username,
+			cognitoId,
+			profilePictureUrl = "i1.jpg",
+			teamId = 1,
+		} = req.body;
+		const newUsers = await prisma.user.create({
+			data: {
+				username,
+				cognitoId,
+				profilePictureUrl,
+				teamId,
+			},
+		});
+		res.json({ message: "User created Successfully", newUsers });
+	} catch (error: any) {
+		res.status(500).json({
+			message: `Error creating user: ${error.message}`,
+		});
+	}
+};
